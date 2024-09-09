4f97b22b18605d75643cb4fef00492ba010ba025
lucasggrama <lucasggrama@yahoo.com>
2024-09-09 14:00:20 -0300

    entidades

--- dolphub/src/main/java/com/cefet/dolphub/Entidades/Main/Professor.java
+++ dolphub/src/main/java/com/cefet/dolphub/Entidades/Main/Professor.java
@@ -1,5 +1,19 @@
 package com.cefet.dolphub.Entidades.Main;
 
-public class Professor extends Usuario{
-    
+import java.util.ArrayList;
+import java.util.ArrayList; // import the ArrayList class
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.AllArgsConstructor;
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "professor", schema = "public")
+public class Professor extends Usuario {
+    @OneToMany(mappedBy = "professor")
+    private ArrayList<Curso> cursos = new ArrayList<>();
 }
